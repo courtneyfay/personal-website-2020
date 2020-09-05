e47f872df5bc997f336a71df4a5bd9371a565152
Courtney Fay <fay.courtney@gmail.com>
2020-09-05 14:38:51 -0600

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,112 @@
+let contentfulConfig
+try {
+  contentfulConfig = require('./.contentful')
+} catch (e) {
+  contentfulConfig = {
+    production: {
+      spaceId: process.env.SPACE_ID,
+      accessToken: process.env.ACCESS_TOKEN,
+    },
+  }
+} finally {
+  const { spaceId, accessToken } = contentfulConfig.production
+  if (!spaceId || !accessToken) {
+    throw new Error('Contentful space ID and access token need to be provided.')
+  }
+}
+
+module.exports = {
+  siteMetadata: {
+    title: 'Courtney Fay',
+    description:
+      'A website where Courtney Fay shares new web development tools, tricks and tips',
+    siteUrl: 'https://gcn.netlify.com',
+    image: '/images/share.jpg',
+    menuLinks: [
+      {
+        name: 'Home',
+        slug: '/',
+      },
+      {
+        name: 'Skills',
+        slug: '/#skills/',
+      },
+      {
+        name: 'Blog',
+        slug: '/blog/',
+      },
+      {
+        name: 'Resume',
+        slug: '/#resume/',
+      },
+      {
+        name: 'Contact',
+        slug: '/contact/',
+      },
+    ],
+    postsPerFirstPage: 31,
+    postsPerPage: 30,
+    basePath: '/',
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    'gatsby-plugin-theme-ui',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+          },
+          `gatsby-remark-autolink-headers`,
+          {
+            resolve: `gatsby-remark-images-contentful`,
+            options: {
+              maxWidth: 650,
+              backgroundColor: 'white',
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: 'gatsby-source-contentful',
+      options:
+        process.env.NODE_ENV === 'development'
+          ? contentfulConfig.development
+          : contentfulConfig.production,
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS,
+        head: true,
+      },
+    },
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'GCN',
+        short_name: 'GCN',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        display: 'minimal-ui',
+        icon: './static/images/favicon.png',
+      },
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-schema-snapshot`,
+      options: {
+        path: `./src/gatsby/schema/schema.gql`,
+        update: process.env.GATSBY_UPDATE_SCHEMA_SNAPSHOT,
+      },
+    },
+    'gatsby-plugin-netlify',
+  ],
+}
